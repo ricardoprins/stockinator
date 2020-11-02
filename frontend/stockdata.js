e669f5f6b04ac34a1d5be699fa02694d65431bda
Ricardo Prins <ricardoprins@gmail.com>
2020-11-02 12:22:37 -0700

    heven - small fix

--- frontend/stockdata.js
+++ frontend/stockdata.js
@@ -1,21 +1,18 @@
-class stockData {
+fetch('https://stockinator-data.herokuapp.com/stock/MSFT').then(function(response){
+                  return response.json();
+                }).then(function(obj){
+                  console.log(obj);
+                  newPrice(obj);
+                }).catch(function(error){
+                  console.error('Something went wrong');
+                  console.error(error);
+                });
 
-    // HTTP Request
-    async get(url) {
-        // Awaiting for response
-        const response = await fetch(url);
-        // Awaiting for response.json()
-        const resData = await response.json();
-        // Returning JSON stock data
-        return resData;
-    }
-
-    async newPrice(map) {
-        currentPrice = arr["open"];
-        highPrice = arr["high"];
-        lowPrice = arr["low"];
-        volumePrice = arr["volume"];
-        var history = document.getElementById("priceHistory");
-        document.getElementById("price").innerHTML = ("OPEN = "+currentPrice+" HIGH ="+highPrice+" LOW ="+lowPrice+" VOLUME ="+volumePrice);
-    }
-}
+                function newPrice(arr){
+                  currentPrice = arr["open"];
+                  highPrice = arr["high"];
+                  lowPrice = arr["low"];
+                  volumePrice = arr["volume"];
+                  var history = document.getElementById("priceHistory");
+                  document.getElementById("price").innerHTML = ("OPEN = "+currentPrice+" HIGH ="+highPrice+" LOW ="+lowPrice+" VOLUME ="+volumePrice);
+                }
