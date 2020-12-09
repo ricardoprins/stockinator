c64f174b21145bc1f4f2dd6839b97577f44b20d1
Ricardo Prins <ricardoprins@gmail.com>
2020-12-08 18:30:13 -0700

    Marcus's increase to backend (#29)
    * test
    
    * espacos
    
    * alt
    
    * alteration
    
    Co-authored-by: MarcusBacke <59940197+MarcusBacke@users.noreply.github.com>
    Co-authored-by: Marcus <marcusbackendorf@gmail.com>

--- backend/stock-data/main.py
+++ backend/stock-data/main.py
@@ -21,3 +21,4 @@ async def get_historical_data(ticker: str, tperiod: str = "1mo"):
     # periods: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
     ticker_data = yf.Ticker(ticker).history(period=tperiod)
     return json.loads(ticker_data.to_json(orient="index", date_format="iso"))
+
