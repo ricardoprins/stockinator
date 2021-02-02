fe546c57901966fc067e084ec4928881ccf4717c
Ricardo Prins <ricardoprins@gmail.com>
2021-02-01 20:24:42 -0700

    Initial new frontend structure

--- frontend/src/js/Title.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Title(){
-    
-    return (
-        <h1>Microsoft</h1>
-    )
-}
--- frontend/src/js/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Title from './Title.js';
-import Stockdata from './Stockdata.js';
-
-ReactDOM.render(
-    <Stockdata />, 
-    document.getElementById('root')
-    )
